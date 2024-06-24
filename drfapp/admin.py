2e09f198df22892461d0e3e0741dd8560888bc9a
-add <temtopeayobami@gmail.com>
2024-06-24 13:26:53 +0100

    Worked on models

--- drfapp/admin.py
+++ drfapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from drfapp.models import Account, Cinema
 
 # Register your models here.
+
+admin.site.register(Account)
+admin.site.register(Cinema)
--- drfapp/models.py
+++ drfapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Account(models.Model):
+    name = models.CharField(max_length=20)
+    mobile = models.CharField(max_length=50)
+    website = models.URLField(max_length=20)
+    
+    def __str__(self):
+        return self.name
+    
+    verbose_name_plural = 'Account'
+    
+    
+class Cinema(models.Model):
+    title = models.CharField(max_length=40)
+    description = models.CharField(max_length=100)
+    active = models.BooleanField(default=True)
+    movie = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="movies")
+    created_at = models.DateTimeField(auto_now_add=True)
